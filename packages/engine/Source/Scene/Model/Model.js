b1941aa4079aad114acfa90e0cd490ce8f551267
lukemckinstry <lukemckinstry@gmail.com>
2025-08-04 13:46:36 -0400

    move nodesByName to scene graph

--- packages/engine/Source/Scene/Model/Model.js
+++ packages/engine/Source/Scene/Model/Model.js
@@ -492,7 +492,6 @@ function Model(options) {
     modelInstances: options.instances,
     model: this,
   });
-  this._nodesByName = {}; // Stores the nodes by their names in the glTF.
 
   /**
    * Used for picking primitives that wrap a model.
@@ -1802,7 +1801,7 @@ Model.prototype.getNode = function (name) {
   Check.typeOf.string("name", name);
   //>>includeEnd('debug');
 
-  return this._nodesByName[name];
+  return this.sceneGraph._nodesByName[name];
 };
 
 /**
